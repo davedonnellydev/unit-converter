5babaa5957d003fc1e10bcfb29e496e0690662f7
Dave Donnelly <davepauldonnelly@gmail.com>
2025-03-22 16:52:41 +1100

    Update calculation approach
    stored each 'to' value in an object, calculations then just update the object. Render function can then be run on the default values (0) or on a value passed through the render function as an argument

--- index.js
+++ index.js
@@ -5,28 +5,46 @@
 */
 
 const convertButton = document.querySelector("#convert-btn");
+const numberInput = document.querySelector("#num-input");
 const metersFeetPara = document.querySelector("#meters-feet-text");
 const litersGallonsPara = document.querySelector("#liters-gallons-text");
 const kilosPoundsPara = document.querySelector("#kilos-pounds-text");
 
+const toValues = {
+    feet: 0,
+    meters: 0,
+    liters: 0,
+    gallons: 0,
+    kilos: 0,
+    pounds: 0
+}
+
 function convertInput() {
-    const inputValue = document.querySelector("#num-input")
+    const fromValue = document.querySelector("#num-input").value
 
-    console.log(metersFeetPara)
-    console.log(litersGallonsPara)
-    console.log(kilosPoundsPara)
+    toValues.feet = Number(fromValue) * 3.28084
+    toValues.meters = Number(fromValue) * 0.3048
+    toValues.liters = Number(fromValue) * 3.78541
+    toValues.gallons = Number(fromValue) * 0.264172
+    toValues.kilos = Number(fromValue) * 0.45359237
+    toValues.pounds = Number(fromValue) * 2.20462
 
-    console.log(inputValue.value)
-
-    let metersFeetText = `${inputValue.value} meters = 65.616 feet | ${inputValue.value} feet = 6.096 meters`
-    let litersGallonsText = `${inputValue.value} liters = 5.284 gallons | ${inputValue.value} gallons = 75.708 liters`
-    let kilosPoundsText = `${inputValue.value} kilos = 44.092 pounds | ${inputValue.value} pounds = 9.072 kilos`
-
-    console.log(metersFeetText)
-    console.log(litersGallonsText)
-    console.log(kilosPoundsText)
-
+    render(fromValue)
 
 }
 
+function render(inputNum = 0) {
+    const metersFeetText = `${inputNum} meters = ${toValues.feet.toFixed(3)} feet | ${inputNum} feet = ${toValues.meters.toFixed(3)} meters`
+    const litersGallonsText = `${inputNum} liters = ${toValues.gallons.toFixed(3)} gallons | ${inputNum} gallons = ${toValues.liters.toFixed(3)} liters`
+    const kilosPoundsText = `${inputNum} kilos = ${toValues.pounds.toFixed(3)} pounds | ${inputNum} pounds = ${toValues.kilos.toFixed(3)} kilos`
+
+    metersFeetPara.textContent = metersFeetText
+    litersGallonsPara.textContent = litersGallonsText
+    kilosPoundsPara.textContent = kilosPoundsText
+
+}
+
+render()
+
 convertButton.addEventListener("click", convertInput);
+numberInput.addEventListener("focus", function() {numberInput.value = null})
